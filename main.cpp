d8da830b68e3bd21d75564c50ad11933fc160510
Smit Contractor <smitcontractor@gmail.com>
2019-02-13 11:47:01 -0800

    task completed

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector<string> list; //array of 5 strings
   int numItems = 0;
   char input;
-
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-
+  int i = 0;
+  string choice;
+  do {
+    cout << "\n==GROCERY LIST MANAGER==";
+    cout << "\nEnter your choice: ";
+    cout << "\n (A)dd an item";
+    cout << "\n (Q)uit";
+    cout << "\nYour choice (A/Q): ";
+    cin >> input;
+    if (input != 'A')
+      break;
+    cout << "enter what you bought" << endl;
+    cin >> choice;
+    list.push_back(choice);
+    i++;
+  } while (i);
+  if (i == 0) {
+    cout << "there are no items on the list." << endl;
+    return 0;
+  }
+  cout << "your shopping list :" << endl;
+  for (int j = 0; j < i; j++) {
+    cout << j + 1 << ". " << list[j] << endl;
+  }
   return 0;
 }
